55719bf0c026fd3ea6887885b9a3a20dc773a85a
carlosmmelo <carlosmmelo@gmail.com>
2016-08-26 13:27:22 -0400

    Testing with Falcon API

---
+++ app.py
@@ -0,0 +1,12 @@
+import falcon
+import images
+
+
+api = application = falcon.API()
+
+storage_path = './'
+image_collection = images.Collection(storage_path)
+image = images.Item(storage_path)
+
+api.add_route('/images', image_collection)
+api.add_route('/images/{name}', image)
